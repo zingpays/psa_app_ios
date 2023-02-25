c6cd7bb3162c46c3280c8ffe03b2a9e8e5a0f0fd
Fei Zhang <fei.zhang@flashwire.com>
2023-02-25 15:13:25 +0800

    Init project

---
+++ PSA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PSA
+//
+//  Created by Fei Zhang on 2023/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-PSA-PSAUITests/Pods-PSA-PSAUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PSA_PSAUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PSA_PSAUITests
+@end
